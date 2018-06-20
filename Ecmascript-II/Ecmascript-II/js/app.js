32b023f3b6737b9462d4dbb26e73182f1aac32a5
Cliente CONTROL-PC <CltControl@GGIECSOFWRK145.espol.edu.ec>
2018-06-20 11:13:38 -0500

    ...

--- Ecmascript-II/Ecmascript-II/js/app.js
+++ Ecmascript-II/Ecmascript-II/js/app.js
@@ -3,7 +3,7 @@
 	function onSuccess(data){
 		console.log(data)
 	}
-
+	/*
 	$.ajax({
 		type: "GET",
 		url: "data/citas.xml",
@@ -11,31 +11,45 @@
 		dataType: "xml",
 		success: onSuccess,
 	})
+	*/
+
+	$.ajax({
+		type: "GET",
+		url: "data/citas.xml",
+		dataType: "xml",
+		success: function(data){
+			$(data).find('cita').each(function(){
+				var lol = $(this).find('texto').text();
+				$("<div></div>").attr('class',"well").html(lol).appendTo("#quotes");
+			});
+		}
+	});
 })();
 
-var elemento3 = document.querySelectorAll('div[class="well"]');
+var elemento3;
 
-$(".buscar").onclick = "buscar()";
+$("#buscar").click(buscar);
+$("#texto").on("keyup",buscar);
 
 function buscar(){
 	textoABuscar = document.getElementById("texto").value;
+	elemento3 = document.querySelectorAll('div[class="well"]');
 	console.log(textoABuscar);
 	console.log(elemento3);
 	if(textoABuscar!=""){
 		for(i of elemento3){
-			if(i.textContent.indexOf(textoABuscar) >= 0){
+			if(i.textContent.includes(textoABuscar)){
+				i.classList.add("mostrar")
 				i.classList.remove("ocultar");
-				i.classList.add("mostrar");
 			}else{
-				i.classList.remove("mostrar")
 				i.classList.add("ocultar");
+				i.classList.remove("mostrar");
 			}
 		}
 	}else{
 		for(i of elemento3){
-			i.classList.remove("ocultar");
 			i.classList.add("mostrar");
 		}
 	}
-	
 }
+
